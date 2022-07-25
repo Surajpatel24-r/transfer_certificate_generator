1451803f40b3df236f107876472151e39d511980
Prakhar Jaiswal <prajai1801@gmail.com>
2022-07-25 07:51:42 +0530

    template

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,26 @@
 import 'package:fluent_ui/fluent_ui.dart';
 import 'package:transfer_certificate_generator/screens/appbase.dart';
+import 'package:transfer_certificate_generator/templates/template_1.dart';
+import 'package:transfer_certificate_generator/templates/template_2.dart';
+import 'package:transfer_certificate_generator/templates/template_3.dart';
 
 void main() {
-  runApp(TransferCertificateGenerator());
+  TemplateCommerce(
+          'Prakhar Jaiswal',
+          'Bharat Lal ',
+          'Vimala Baghel',
+          'Male',
+          '18-03-2001',
+          'B.Tech(Computer Science and Engineering)',
+          '1st Semester',
+          '6th Semester',
+          'BH3404 / 300202219043',
+          'Passed',
+          'Asshish ki biwi ka case ko dekh laga ab placement to milne se raha',
+          'conduct',
+          '25-06-2001')
+      .createPdf();
+  // runApp(TransferCertificateGenerator());
 }
 
 class TransferCertificateGenerator extends StatelessWidget {
